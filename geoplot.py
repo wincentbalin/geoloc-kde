4c12987d8b3578d953ec6fa907a2a358f1bd885d
Wincent Balin <wincent.balin@gmail.com>
2022-04-06 02:05:34 +0200

    Run with Python 3

--- geoplot.py
+++ geoplot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #####################################################################################
 # Simple map plotting of --print-matrix output from geoloc                          #
